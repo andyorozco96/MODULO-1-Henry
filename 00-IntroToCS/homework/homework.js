d3f7cfeda92e5af2f700e26db8a9f33442e3dab3
Andrés Orozco <orozco.andres.dev@gmail.com>
2022-03-28 15:06:23 -0300

    Ejercicios 00-INTRO resueltos

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,12 +2,34 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  /*
+  1) Invertir el numero brindado en num
+
+  */
+
+let numReverse = num.split("").reverse();
+let decimal = 0;
+let suma;
+
+ for (let i = 0; i < numReverse.length; i++){
+    suma = (numReverse[i] * Math.pow(2,i));
+    decimal = suma + decimal;
+ }
+
+ return decimal;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  
+  
+  let binarioFinal = [];
+  while (num !== 0) {
+    binarioFinal.push(num % 2);
+    num = Math.floor(num / 2);
+  } 
+  return binarioFinal.reverse().join("");
 }
 
 
